464eacb428f42bd63a7be493291ef64374a1dbe8
mildmojo <mildmojo@users.noreply.github.com>
2018-02-08 00:36:31 -0500

    Adds emission color to lamp flicker script.

--- Assets/scripts/ColorFlicker.cs
+++ Assets/scripts/ColorFlicker.cs
@@ -6,22 +6,29 @@ namespace Airmail {
 
   public class ColorFlicker : MonoBehaviour {
     public Color colorTo;
+    public Color emissionTo;
     public float dutyCycle;
     public float time;
 
     private Color colorFrom;
+    private Color emissionFrom;
     private MeshRenderer _renderer;
     private bool _isFlickering;
 
     void Awake() {
       _renderer = GetComponent<MeshRenderer>();
       colorFrom = _renderer.material.color;
+      emissionFrom = _renderer.material.GetColor("_EmissionColor");
     }
 
-    public void Flicker() {
+    public void StartFlicker() {
       if (_isFlickering) return;
 
-      StartCoroutine(doFlicker());
+      StartCoroutine("doFlicker");
+    }
+
+    public void StopFlicker() {
+      StopCoroutine("doFlicker");
     }
 
     IEnumerator doFlicker() {
@@ -33,9 +40,12 @@ namespace Airmail {
 Debug.Log("Flickering!");
       while (elapsedTime < 5f) {
         var delay = Random.Range(0.1f, 1f);
-        elapsedTime += delay;
         yield return new WaitForSeconds(delay);
+        elapsedTime += delay;
         _renderer.material.color = _renderer.material.color == colorFrom ? colorTo : colorFrom;
+        var oldEmission = _renderer.material.GetColor("_EmissionColor");
+        var newEmission = oldEmission == emissionFrom ? emissionTo : emissionFrom;
+        _renderer.material.SetColor("_EmissionColor", newEmission);
       }
 Debug.Log("Not flickering!");
       _isFlickering = false;
